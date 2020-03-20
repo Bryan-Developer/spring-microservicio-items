f7614ee999f0737c9b7cf3137337723c42b2cdb5
Bryan Antonio Alvarado Villalobos <bryan.developer.alvarado@gmail.com>
2020-03-20 00:43:58 -0500

    Agregando base del repositorio

---
+++ src/main/java/com/bryan/developer/curso/microservicio/items/SpringMicroservicioItemsApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.developer.curso.microservicio.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicioItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicioItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryan/developer/curso/microservicio/items/SpringMicroservicioItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.developer.curso.microservicio.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicioItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
